f564f2236f65a53af4ed9edf0f3cce5da78fef34
Matt Gordon <matt@mattrgordon.com>
2024-07-21 16:19:57 -0500

    adding functionality to inquirer

--- server.js
+++ server.js
@@ -1,19 +1,91 @@
 const inquirer = require('inquirer');
 const prompts = require('./prompts');
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const pool = new Pool(
+  {
+    user: 'postgres',
+    password: 'admin',
+    host: 'localhost',
+    database: 'employees_db',
+  },
+  console.log('Connected!')
+);
+
+pool.connect();
+
+async function getDepartments() {
+  try {
+    const response = await pool.query(`SELECT name FROM department`);
+    console.table(response.rows);
+  } catch (error) {
+    console.error('There was an error getting departments', error);
+    throw error;
+  }
+}
+
+async function addDepartments(name) {
+  try {
+    const response = await pool.query(`INSERT INTO department (${name})`);
+    console.table(response.rows);
+  } catch (error) {
+    console.error('There was an error adding a department', error);
+    throw error;
+  }
+}
+
+async function getRoles() {
+  try {
+    const response = await pool.query(
+      `SELECT title, id, salary, department FROM role`
+    );
+    console.table(response.rows);
+  } catch (error) {
+    console.error('There was an error getting roles', error);
+    throw error;
+  }
+}
+
+async function getEmployees() {
+  try {
+    const response = await pool.query(
+      `SELECT id, first_name, last_name, role_id, manager_id FROM employee`
+    );
+    console.table(response.rows);
+  } catch (error) {
+    console.error('There was an error getting employees', error);
+    throw error;
+  }
+}
 
 function init() {
   inquirer.prompt(prompts).then((response) => {
     switch (response?.question1) {
       case 'viewDepartments':
-        console.log('view Departments');
+        getDepartments().then(() => init());
         break;
       case 'viewRoles':
-        console.log('view roles');
+        getRoles().then(() => init());
         break;
       case 'viewEmployees':
-        console.log('view employees');
+        getEmployees().then(() => init());
         break;
       case 'addDepartment':
+        inquirer
+          .prompt({
+            message: 'Enter name of the department',
+            name: 'department',
+          })
+          .then((deptRes) => {
+            console.log('res', deptRes);
+            addDepartments(deptRes.department);
+          });
         console.log('add department');
         break;
       case 'addRole':
@@ -25,6 +97,8 @@ function init() {
       case 'updateEmployeeRole':
         console.log('update employee role');
         break;
+      case 'quit':
+        process.exit(0);
       default:
         console.log('there was an error');
         break;

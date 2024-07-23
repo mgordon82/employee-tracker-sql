185a2ce2aec26b8a33d4d2e4e52d0f0153bc11d0
Matt Gordon <matt@mattrgordon.com>
2024-07-23 00:38:34 -0500

    constants file for options

---
+++ prompts/actions.js
@@ -0,0 +1,17 @@
+const VIEW_DEPARTMENTS = 'View All Departments';
+const VIEW_ROLES = 'View All Roles';
+const VIEW_EMPLOYEES = 'View All Employees';
+const ADD_DEPARTMENT = 'Add a Department';
+const ADD_ROLE = 'Add a Role';
+const ADD_EMPLOYEE = 'Add an Employee';
+const QUIT_OPTION = 'Quit';
+
+module.exports = {
+  VIEW_DEPARTMENTS,
+  VIEW_EMPLOYEES,
+  VIEW_ROLES,
+  ADD_DEPARTMENT,
+  ADD_EMPLOYEE,
+  ADD_ROLE,
+  QUIT_OPTION,
+};
